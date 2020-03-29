7a92bbde32d6c3d0f0f8edf5288b93915048e4ab
lizhongyuan <lizhongyuan@cnbmtecth.com>
2020-03-29 19:18:00 +0800

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+const { BaseLogger } = require('./lib/logger');
+const Middleware = require('./lib/middleware/middleware');
+const { globalWinstonInstance } = require('./lib/service/winston');
+
+
+module.exports = {
+  BaseLogger,
+  globalWinstonInstance,
+  Middleware
+};
